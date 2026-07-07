580743015baed86c3ea5c0289d3a2559cfa910d6
Christopher <christopheryzl@gmail.com>
2026-07-07 16:32:40 +0100

    added .asv file type to gitignore
    reworked readFromList.m, trying to generalise returned table to accomodate for different h5 datasets
    
    probably get Claude to refactor this into multiple smaller functions and call as needed, since some datasets may not have load cell/encoder/metadata

--- parse h5 file/readFromList.m
+++ parse h5 file/readFromList.m
@@ -1,4 +1,4 @@
-function [Results] = readFromList(fileName,selectedGroups,varargin)
+function [Results] = readFromList(fileName,groups,varargin)
 % read results stored as a .mat from selected Groups from the h5 dataset
 
 % optional arguments:
@@ -15,75 +15,105 @@ else
     % this part is hard-coded, could be changed so that the mic list is read from the h5 group directly 
     MicList = 1:58;
 end
-
 microphones = "mic"+strtrim(string(num2str([MicList]')));
 
-Output = cell(length(selectedGroups),9);
+%% initialise output cells
+% make case attribute variable names
+attrsName = string({groups(1).Attributes.Name});
+% make case attribute cell
+attrsCell = cell(length(groups),length(attrsName));
 
-for i = 1:length(selectedGroups)
-    name = selectedGroups(i);
-    Output{i,1} = h5readatt(fileName,name,"advance ratio");
-    Output{i,2} = h5readatt(fileName,name,'separation');
-    Output{i,3} = h5readatt(fileName,name,'blade number');
-    Output{i,4} = h5readatt(fileName,name,'inflow');
-    Output{i,5} = h5readatt(fileName,name,'rotational speed');
-    Output{i,6} = h5readatt(fileName,name,"phase");
-   
-    BPF = Output{i,3}*Output{i,5}/60;
-    phi = zeros(length(microphones),1);
-    theta = phi;
-    r = phi;
-    Fs = phi;
-    OASPL1 = phi;
-    OASPL4 = phi;
+noiseData = cell(length(groups),1);
+data_enc = noiseData;
+
+% make microphone variable names
+micFirstGroup =find(strcmp({groups(1).Groups.Name},groups(1).Name+"/"+microphones(1)));
+micsAttrsName = string({groups(1).Groups(micFirstGroup).Attributes.Name});
+
+% iterate over all groups
+for i = 1:length(groups)
+    thisGroup = groups(i);
+    % make attribute cells
+    for j = 1:length(thisGroup.Attributes)
+        attrsCell{i,j} = readAttrsByName(thisGroup,attrsName(j));
+    end
     
     data1 = cell(length(microphones),1);
     data4 = data1;
-    harmonic_data1 = data1;
+    
+    % make microphone attributes cell
+    micAttrsCell = cell(length(microphones),length(micsAttrsName));
 
     for j = 1:length(microphones)
-        groupName = name+"/"+microphones(j);
-        SPL1 = h5read(fileName,groupName+"/1dF/SPL");
+        micMask =strcmp({thisGroup.Groups.Name},thisGroup.Name+"/"+microphones(j));
+        thisMic = thisGroup.Groups(micMask);
 
-        F1 = h5read(fileName,groupName+"/1dF/F");
+        % make microphone attributes cell
+        for k = 1:length(thisMic.Attributes)
+            micAttrsCell{j,k} = readAttrsByName(thisMic,micsAttrsName(k));
+        end
+        % create name mask for df1 group
+        nameMask = contains({thisMic.Groups.Name},"df1",IgnoreCase=true);
+        df1 = thisMic.Groups(nameMask);
         
-        SPL4 = h5read(fileName,groupName+"/4dF/SPL");
+        SPL1        = readGroupByName(df1,"spl");
+        F1          = readGroupByName(df1,"f");
+        PSD1        = readGroupByName(df1,"psd");
+        OASPL1(j)   = readGroupByName(df1,"oaspl");
+        
+        data1{j} = table(F1,PSD1,SPL1,VariableNames=["f","psd","spl"]);
+        
+        % create name mask for df4 group
+        nameMask = contains({thisMic.Groups.Name},"df4",IgnoreCase=true);
+        df4 = thisMic.Groups(nameMask);
+        SPL4        = readGroupByName(df4,"spl");
+        F4          = readGroupByName(df4,"f");
+        PSD4        = readGroupByName(df4,"psd");
+        OASPL4(j)   = readGroupByName(df4,"oaspl");
 
-        F4 = h5read(fileName,groupName+"/4dF/F");
-        OASPL1(j) = h5read(fileName,groupName+"/1dF/OASPL");
-        OASPL4(j) = h5read(fileName,groupName+"/4dF/OASPL");
-
-        phi(j) = h5readatt(fileName,name+"/"+microphones(j),"phi (deg)");
-        theta(j) = h5readatt(fileName,name+"/"+microphones(j),"theta (deg)");
-        r(j) = h5readatt(fileName,name+"/"+microphones(j),"r (m)");
-        Fs(j) = 65536;%h5readatt(fileName,name+"/"+microphones(j),"sampling frequency");
-        data1{j} = table(F1,SPL1,VariableNames=["F","SPL"]);
-        data4{j} = table(F4,SPL4,VariableNames=["F","SPL"]);
-
-        [BPHSPL,~,~] = findFirst5BPH(F1,SPL1,BPF,1);
-        harmonic_data1{j} = table([1:5]',BPHSPL',VariableNames=["harmonic","SPL"]);
+        data4{j} = table(F4,PSD4,SPL4,VariableNames=["f","psd","spl"]);
     end
     
 
-    data = table(phi,theta,r,Fs,OASPL1,OASPL4,data1,data4,harmonic_data1,VariableNames=["phi","theta","r","Fs","OASPL1","OASPL4","dF1","dF4","Harmonics"]);
-    Output{i,7} = data;
-    %read encoder data from Forrest's dataset
-    inboard_enc = h5read(fileName,name+"/inboard/Phase");
-    outboard_enc = h5read(fileName,name+"/outboard/Phase");
+    noiseData{i} = table(OASPL1',OASPL4',data1,data4,VariableNames=["OASPL1","OASPL4","dF1","dF4"]);
+    %read encoder data
 
-    data_enc = table(inboard_enc,outboard_enc,VariableNames=["inboard","outboard"]);
-    Output{i,8} = data_enc;
+    % create mask for front motor
+    motorMask = contains({thisGroup.Groups.Name},"front");
+    front = thisGroup.Groups(motorMask);
+    front_enc = readGroupByName(front,"angle");
 
-    %read load cell data from Forrest's dataset
-    inboard_LC = h5read(fileName,name+"/inboard/Fz");
-    outboard_LC = h5read(fileName,name+"/outboard/Fz");
-    inboard_LC = mean(inboard_LC);
-    outboard_LC = mean(outboard_LC);
-    
-    data_LC = table(inboard_LC,outboard_LC,VariableNames=["inboard","outboard"]);
-    Output{i,9} = data_LC;
-end
-Results = cell2table(Output,VariableNames=["J","Sep","B_No","inflow","rpm","phase","noise data","encoder data","load cell data"]);
+    % create mask for rear motor
+    motorMask = contains({thisGroup.Groups.Name},"rear");
+    rear = thisGroup.Groups(motorMask);
+    rear_enc = readGroupByName(rear,"angle");
+
+    data_enc{i} = table(front_enc,rear_enc,VariableNames=["front","rear"]);
+
 
 end
+attrsTable = cell2table(attrsCell,VariableNames=attrsName);
+micAttrsTable = cell2table(micAttrsCell,VariableNames=micsAttrsName);
+for i = 1:length(noiseData)
+    noiseData{i} = [micAttrsTable,noiseData{i}];
+end
+micTable = cell2table(noiseData,VariableNames="noise data");
+encoderDataTable = cell2table(data_enc,VariableNames="encoder data");
+
+data_table = [micTable,encoderDataTable];
+Results = [attrsTable,data_table];
+
+
+    function [out] = readGroupByName(group,datasetName)
+        mask = strcmp({group.Datasets.Name},datasetName);
+        out = h5read(fileName,group.Name+"/"+group.Datasets(mask).Name);
+
+    end
+
+    function [out] = readAttrsByName(group,attrsName)
+        mask = strcmp({group.Attributes.Name},attrsName);
+        out = group.Attributes(mask).Value;
+    end
+end
+
 
