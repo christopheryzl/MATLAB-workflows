95f865f6e880eb5685164a1efb461376a3639774
Christopher <christopheryzl@gmail.com>
2026-07-13 13:51:50 +0100

    Added function selectiveBin, bins acoustic pressure from raw table processed by processRawTable. I haven't decided on the output structure yet but results could be useful for cwt, ensemble averaging, source decomposition, etc

---
+++ process mat file/selectiveBin.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = selectiveBin(inputArg1,inputArg2)
+%SELECTIVEBIN Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
